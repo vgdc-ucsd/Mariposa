c0f2f7d02c52bd49eab6b154e51d7771fae83d0a
AnApostrophe <TheStellarUniverse@gmail.com>
2025-01-22 17:13:12 -0800

    Additional File Organization

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private void Awake()
+	{
+
+	}
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+
+	private void FixedUpdate()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Systems/Singleton.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+	public static T Instance;
+
+    public virtual void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this as T;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+}
