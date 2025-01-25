e7aaa91e6affcfa9f93426e6779405e6e004a9a4
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2025-01-25 15:32:30 -0800

    Changed the player from a kinematic rigidbody to a dynamic rigidbody. This required a major overhaul to the player movement, so I ended up removing the abstract body and freebody classes and moving the behavior entirely to the playerMovement script. Player movement largely works the same as before, but the built-in physics system now handles the physical collision detection.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,42 +1,122 @@
+using NUnit.Framework.Internal.Commands;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
+// The external forces this object can currently experience 
 
-
-public class PlayerMovement : FreeBody
+public enum BodyState
 {
-    public float MoveSpeed = 10;
+    OnGround, // Experiences sliding friction
+    InAir, // Experiences gravity, (horizontal) air friction, and terminal drag
+    Locked  // Cannot move and does not experience any force
+}
 
+public class PlayerMovement : MonoBehaviour
+{
     public static PlayerMovement Instance;
 
-    protected override void Awake()
+    private Rigidbody2D rb;
+    private Collider2D col;
+    [SerializeField] private LayerMask playerLayer;
+
+    public float MoveSpeed = 10;
+    [SerializeField] private float TerminalVelocity; // the maximum fall velocity
+    public float Gravity;
+
+    private const float COLLISION_CHECK_DISTANCE = 0.05f; // how far away you have to be from a surface to be considered "colliding" with it
+    private const float LAND_SLOPE_FACTOR = 0.9f; // magnitude of the y component of a surface normal to be considered vertical
+
+    public BodyState State;
+
+    private Vector2 velocity; // the velocity for the current frame
+    private float fdt; // Shorthand for fixed delta time
+
+    private void Awake()
     {
-        base.Awake();
+        col = GetComponent<Collider2D>();
+        rb = GetComponent<Rigidbody2D>();
+
+        State = BodyState.InAir; // Initialize the player as in the air
+
         Instance = this;
     }
 
-    protected override void Update()
+    private void FixedUpdate()
     {
-        base.Update();
+        fdt = Time.fixedDeltaTime;
 
-        // very temporary code until we get proper input handling set up
         if (Input.GetKey(KeyCode.D)) Move(1);
         else if (Input.GetKey(KeyCode.A)) Move(-1);
         else Move(0);
 
-        if (Input.GetKey(KeyCode.Space)) Jump(100);
+        if (Input.GetKey(KeyCode.Space)) Jump();
 
+        Fall();
+        CheckGrounded();
+        CheckCeilingHit();
+        ApplyFriction();
 
+        UpdateVelocity();
     }
 
     // Move by player input: 1 = right, -1 = left
     private void Move(int dir)
     {
-        Velocity.x = MoveSpeed * dir;
+        velocity.x = MoveSpeed * dir;
     }
 
-    private void Jump(int dir)
+    // Directly set the player's y velocity
+    private void Jump()
     {
-        Velocity.y = MoveSpeed * dir; 
+        velocity.y = MoveSpeed; // this should be its own value
     }
 
+    // Apply gravity in the air
+    private void Fall()
+    {
+        if (State != BodyState.InAir) return;
+
+        velocity.y = Mathf.Max(velocity.y - Gravity * fdt, -TerminalVelocity);
+    }
+
+    // Check if the player is about to collide with the ground and update the player's bodyState accordingly
+    private void CheckGrounded()
+    {
+        RaycastHit2D groundHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.down, COLLISION_CHECK_DISTANCE, ~playerLayer);
+
+        if (State != BodyState.OnGround && groundHit && groundHit.normal.normalized.y > LAND_SLOPE_FACTOR)
+        {
+            State = BodyState.OnGround;
+            velocity.y = 0;
+        }
+        else if (State == BodyState.OnGround && !groundHit)
+        {
+            State = BodyState.InAir;
+        }
+    }
+
+    // Make the player fall immediately after hitting a ceiling
+    private void CheckCeilingHit()
+    {
+        // Similar raycast to CheckGrounded
+        RaycastHit2D ceilingHit = Physics2D.BoxCast(col.bounds.center, col.bounds.size, 0f, Vector2.up, COLLISION_CHECK_DISTANCE, ~playerLayer);
+
+        if (ceilingHit && -ceilingHit.normal.normalized.y > LAND_SLOPE_FACTOR) velocity.y = Mathf.Min(0, velocity.y);
+    }
+
+    private void ApplyFriction()
+    {
+        if (State == BodyState.OnGround)
+        {
+            // ...
+        }
+        else if (State == BodyState.InAir)
+        {
+            // ...
+        }
+    }
+
+    // Apply any changes to the velocity to the actual rigidbody velocity
+    private void UpdateVelocity() => rb.linearVelocity = velocity;
 }
