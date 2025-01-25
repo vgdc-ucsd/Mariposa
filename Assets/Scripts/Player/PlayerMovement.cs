f2321df5c6812965deefc86da2f48f4c0f462c25
ATwistedJoke <vroatis06@gmail.com>
2025-01-24 20:00:34 -0800

    Temporary Jump Button
    This does not work correctly yet.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -13,12 +13,22 @@ public class PlayerMovement : FreeBody
         // very temporary code until we get proper input handling set up
         if (Input.GetKey(KeyCode.D)) Move(1);
         else if (Input.GetKey(KeyCode.A)) Move(-1);
+        else Move(0);
+
+        if (Input.GetKey(KeyCode.Space)) Jump(100);
+
+
     }
 
     // Move by player input: 1 = right, -1 = left
     private void Move(int dir)
     {
         Velocity.x = MoveSpeed * dir;
+    }
+
+    private void Jump(int dir)
+    {
+        Velocity.y = MoveSpeed * dir; 
     }
 
 }
