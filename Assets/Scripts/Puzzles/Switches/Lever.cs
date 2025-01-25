f06b75f8af74547093cf9117d96ce307207a1eca
varsha106-pixel <varshajawadi@gmail.com>
2025-01-24 21:29:05 -0800

    Update Lever.cs
    overrided triggerswitch from the switch class and checked if lever is pulled or not. If lever is pulled it is pushed back (isPulled set to false) otherwise if it isn't already is gets pulled (isPulled set to true)

--- Assets/Scripts/Puzzles/Switches/Lever.cs
+++ Assets/Scripts/Puzzles/Switches/Lever.cs
@@ -4,7 +4,27 @@ using UnityEngine;
 /// </summary>
 public class Lever  : Switch
 {
-    public bool isPulled;
+    /// <summary>
+   /// boolean for if lever is pulled or not
+   /// </summary>
+   public bool isPulled;
+   
+   /// <summary>
+   /// Override of TriggerSwitch from switch class which checks if lever is 
+   /// pulled and either pushes it back up or pulls it if unpulled
+   /// when pulled lever should "do some action" that will be implemented later
+   /// </summary>
+   
+   @Override
+   public abstract void TriggerSwitch() 
+   {
+    if(isPulled)
+    {
+        isPulled=false;
+    }
+    isPulled=true;
     
-    
+   }
+
+
 }
