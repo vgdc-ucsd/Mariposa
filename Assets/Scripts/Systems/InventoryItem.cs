2d558148409fdabba99c971195862c809487dfa0
chris8332558 <chris8332558@gmail.com>
2025-01-31 19:26:28 -0800

    Made InvetoryItem an ScriptableObject
    Also Create two test InvetoryItemSO in ScriptableObject/InvetoryItems/

--- Assets/Scripts/Systems/InventoryItem.cs
+++ Assets/Scripts/Systems/InventoryItem.cs
@@ -6,12 +6,15 @@ public enum InventoryItemType {
 	Memento
 }
 
-public class InventoryItem 
+
+[CreateAssetMenu(fileName = "InventoryItem", menuName = "InventoryItem")]
+public class InventoryItem : ScriptableObject
 {
 	public string Name;
 	public string FlavorText;
 	public uint ID; 
 	public InventoryItemType Type;
+	public Sprite sprite;
 
 	/// <summary>
 	/// Creates a record representing an item. 
