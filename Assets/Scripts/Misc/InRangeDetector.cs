e01fcb7a67710aeda3d64f0cea09edac8430d57a
chris8332558 <chris8332558@gmail.com>
2025-02-05 20:11:08 -0800

    Fix InRangeDetector bug
    Also make the turret a prefab

--- Assets/Scripts/Misc/InRangeDetector.cs
+++ Assets/Scripts/Misc/InRangeDetector.cs
@@ -10,16 +10,25 @@ public class InRangeDetect : MonoBehaviour
     [Header("View Range")]
     [SerializeField] [Range(1, 30)] float sizeX;
     [SerializeField] [Range(1, 30)] float sizeY;
-    [SerializeField] [Range(0, 10)] float offsetX;
-    [SerializeField] [Range(0, 10)] float offsetY;
+    [SerializeField] [Range(-10, 10)] float offsetX;
+    [SerializeField] [Range(-10, 10)] float offsetY;
+
+    [Header("Gizmo Color")]
+    [SerializeField] Color inRange;
+    [SerializeField] Color notInRange;
+
+    public float SizeX => sizeX;
+    public float SizeY => sizeY;
+    public float OffsetX => offsetX;
+    public float OffsetY => offsetY;
 
     public bool IsTargetInRange()
     {
         float targetX = target.transform.position.x;
         float targetY = target.transform.position.y;
-        float minX = transform.position.x - offsetX - (sizeX / 2);
+        float minX = transform.position.x + offsetX - (sizeX / 2);
         float maxX = transform.position.x + offsetX + (sizeX / 2);
-        float minY = transform.position.y - offsetY - (sizeX / 2);
+        float minY = transform.position.y + offsetY - (sizeY / 2);
         float maxY = transform.position.y + offsetY + (sizeY / 2);
         bool isPlayerInRange = (targetX > minX && targetX < maxX) && (targetY > minY && targetY < maxY);
         return isPlayerInRange;
@@ -27,9 +36,13 @@ public class InRangeDetect : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        if (Application.isPlaying && IsTargetInRange()) Gizmos.color = Color.red;
-        Vector3 center = new Vector3(transform.position.x + offsetX, transform.position.y + offsetY, 0);
-        Vector3 size = new Vector3(sizeX, sizeY, 0);
-        Gizmos.DrawWireCube(center, size);
+        if (target != null)
+        {
+            if (IsTargetInRange()) Gizmos.color = inRange;
+            else Gizmos.color = notInRange;
+            Vector3 center = new Vector3(transform.position.x + offsetX, transform.position.y + offsetY, 0);
+            Vector3 size = new Vector3(sizeX, sizeY, 0);
+            Gizmos.DrawWireCube(center, size);
+        }
     }
 }
