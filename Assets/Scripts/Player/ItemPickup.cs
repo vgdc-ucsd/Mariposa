51e98e199ae42458852aea38e85a1d5ef6c5b7fd
ATwistedJoke <vroatis06@gmail.com>
2025-02-07 18:30:17 -0800

    Added Jump Mechanics
    Added a gravity change onto the player's falling speed and added a Double Jump.

--- Assets/Scripts/Player/ItemPickup.cs
+++ Assets/Scripts/Player/ItemPickup.cs
@@ -24,7 +24,7 @@ public class ItemPickup : MonoBehaviour
     // Makes the object disappear and no longer have effects in game if the player collides with it
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name == Player.ActivePlayer.gameObject)
+        if(collision.gameObject.name == Player.ActivePlayer.gameObject.name)
         {
             Debug.Log(gameObject.name + " collected by Player");
             gameObject.SetActive(false);
