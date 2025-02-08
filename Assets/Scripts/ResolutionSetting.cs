d6ce4fb1b323b8d8992c418bdc4f60d3ed1fabee
aiden3918 <liaiden3918@gmail.com>
2025-02-07 20:14:13 -0800

    resolution now includes windowedfullscreen
    resolution now includes windowedfullscreen and yoiu are now able to use other resolutions while in fullscreen

--- Assets/Scripts/ResolutionSetting.cs
+++ Assets/Scripts/ResolutionSetting.cs
@@ -1,17 +1,28 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
+using UnityEngine.UIElements.Experimental;
+
+/// <summary>
+/// Unity moment
+/// </summary>
+public enum WindowType
+{
+    Windowed,
+    Fullscreen,
+    WindowedFullscreen
+}
 
 public class ResolutionSetting : MonoBehaviour
 {
 
-    private int[,] resolutions = {{640, 360}, {1280, 720}, {1600, 900}, {1920, 1080}, {2560, 1440}, {3840, 2160}};
+    private readonly int[,] resolutions = {{640, 360}, {1280, 720}, {1600, 900}, {1920, 1080}, {2560, 1440}, {3840, 2160}};
     public int currentResolutionIndex = 0;
 
 
     public int Width;
     public int Height;
-    public bool Fullscreen = true;
+    public WindowType ResolutionType = WindowType.Windowed;
 
     /// <summary>
     /// On program start, sets default resolution (may want to edit this if settings are saved)
@@ -20,45 +31,39 @@ public class ResolutionSetting : MonoBehaviour
     {
         Width = resolutions[currentResolutionIndex,0];
         Height = resolutions[currentResolutionIndex,1];
-        Screen.SetResolution(Width, Height, false);
+        Screen.SetResolution(Width, Height, FullScreenMode.Windowed);
     }
 
     /// <summary>
-    /// Changes resolution when button to apply changes is clicked
+    /// Saves applied resolution to class when resolutionObj is clicked
     /// </summary>
-    /// <param name="dropdown">the dropdown menu GameObject</param>
-    public void SetRes(GameObject dropdown)
+    /// <param name="resolutionObj">the dropdown menu GameObject</param>
+    public void SaveResolutionDimensions(GameObject resolutionObj)
     {
-        if (Fullscreen)
-        {
-            Screen.SetResolution(Display.main.systemWidth, Display.main.systemHeight, Screen.fullScreen);
-            return;
-        }
-
-        currentResolutionIndex = dropdown.GetComponent<Dropdown>().value;
+        currentResolutionIndex = resolutionObj.GetComponent<Dropdown>().value;
         Width = resolutions[currentResolutionIndex, 0];
         Height = resolutions[currentResolutionIndex, 1];
-        Screen.SetResolution(Width, Height, false);
-        Debug.Log(Screen.currentResolution);
     }
 
     /// <summary>
-    /// Updates Fullscreen field when checkbox is changed
+    /// Saves resolution type to class when resolutionObj is clicked
     /// </summary>
-    /// <param name="checkbox">the toggle fulscreen checkbox GameObject</param>
-    public void UpdateSettingsOnFullscreenToggle(GameObject checkbox)
+    /// <param name="fullScreenObj">the dropdown menu GameObject</param>
+    public void SaveResolutionType(GameObject fullscreenObj)
     {
-        Fullscreen = checkbox.GetComponent<Toggle>().isOn;
+        ResolutionType = (WindowType)fullscreenObj.GetComponent<Dropdown>().value;
     }
 
     /// <summary>
-    /// changes interactibility of resolutions dropdown menu, called right after UpdateSettingsOnFullscreenToggle
+    /// Finally apply changes to resolution when resolutionObj is clicked
     /// </summary>
-    /// <param name="dropdown">the dropdown menu GameObject</param>
-    public void ResolutionReaction(GameObject dropdown)
+    public void ApplyResolutionChanges()
     {
-        dropdown.GetComponent<Dropdown>().interactable = !Fullscreen;  
+        switch (ResolutionType)
+        {
+            case WindowType.Windowed: Screen.SetResolution(Width, Height, FullScreenMode.Windowed); return;
+            case WindowType.Fullscreen: Screen.SetResolution(Width, Height, FullScreenMode.ExclusiveFullScreen); return;
+            case WindowType.WindowedFullscreen: Screen.SetResolution(Width, Height, FullScreenMode.FullScreenWindow); return;
+        }
     }
-
-
 }
