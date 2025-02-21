a9b3206a21284eab5b378c1d5b11a31d20fd815f
aodintsov1 <aodintsov4edu@gmail.com>
2025-02-21 15:30:35 -0800

    Battery respawns if player's battery count is 0

--- Assets/Scripts/Systems/BatteryPickup.cs
+++ Assets/Scripts/Systems/BatteryPickup.cs
@@ -1,8 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class BatteryPickup : MonoBehaviour
 {
     [SerializeField] private BatteryItem batteryItem;
+    [SerializeField] private float checkInterval = 1f;
+    private Vector3 spawnPosition;
+    
+    private SpriteRenderer sr;
+    private Collider2D col;
+    
+    private void Awake()
+    {
+        spawnPosition = transform.position;
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
     
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -16,7 +29,21 @@ public class BatteryPickup : MonoBehaviour
             {
                 Debug.LogError("InventoryManager.Instance or batteryItem is null!");
             }
-            Destroy(gameObject);
+            sr.enabled = false;
+            col.enabled = false;
+            
+            StartCoroutine(RespawnWhenBatteryCountZero());
         }
+    }
+    
+    private IEnumerator RespawnWhenBatteryCountZero()
+    {
+        while(InventoryManager.Instance.GetItemCount(batteryItem) > 0)
+        {
+            yield return new WaitForSeconds(checkInterval);
+        }
+        transform.position = spawnPosition;
+        sr.enabled = true;
+        col.enabled = true;
     }
 }
