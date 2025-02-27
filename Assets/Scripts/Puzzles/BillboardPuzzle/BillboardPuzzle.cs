4dede4a99b01ac0b0eb8be8127461cf7f6ab417f
NickMitroff <nmitroff@ucsd.edu>
2025-02-26 22:44:39 -0800

    Initial commit

---
+++ Assets/Scripts/Puzzles/BillboardPuzzle/BillboardPuzzle.cs
@@ -0,0 +1,162 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using System.Linq;
+using static UnityEngine.Rendering.DebugUI.Table;
+
+public class BillboardPuzzle : Puzzle
+{
+    public static BillboardPuzzle Instance;
+    public BillboardPuzzleTile[,] board;
+    public GameObject[] boardDisplay;
+    private int width = 5;
+    private int height = 3;
+    private List<int> endXValues = new List<int>();
+    private List<int> endRotValues = new List<int>();
+    [SerializeField] private Sprite[] sprites;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Debug.Log("Tried to create more than one instance of the BillboardPuzzle singleton!");
+        }
+    }
+
+    private void Start()
+    {
+        board = new BillboardPuzzleTile[height, width];
+        int index = 0;
+        GenerateSolution();
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+
+                board[i, j].x = j;
+                board[i, j].endX = endXValues[index];
+                board[i, j].spriteIndex = board[i, j].endX;
+                board[i, j].rotation = endRotValues[index];
+                index++;
+            }
+        }
+        UpdatePuzzleDisplay();
+    }
+
+    public void ShiftRow(int row)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            board[row, i].x++;
+            board[row, i].x %= width;
+        }
+        UpdatePuzzleDisplay();
+    }
+
+
+    public void RotateCol(int col)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (board[i, j].x != col) continue;
+                if (col % 2 == 0) board[i, j].rotation--;
+                else board[i, j].rotation++;
+                board[i, j].rotation %= 4;
+            }
+        }
+        UpdatePuzzleDisplay();
+    }
+
+    /// <summary>
+    /// Procedurally generates a solution to the puzzle.
+    /// To prevent the puzzle from being mathematically impossible, the sums of the rotation values of each row are made to be equal.
+    /// </summary>
+    private void GenerateSolution()
+    {
+        int targetSum = 10;
+        for (int i = 0; i < height; i++)
+        {
+            int rotSum = 0;
+            int[] rotRow = new int[width];
+            int shiftX = Random.Range(0, width);
+            List<int> rowXValues = new List<int>();
+            for (int j = 0; j < width; j++)
+            {
+                rowXValues.Add(j);
+                int rot = Random.Range(0, 4);
+                rotRow[j] = rot;
+                rotSum += rot;
+            }
+
+            while (rotSum != targetSum)
+            {
+                int rIndex = Random.Range(0, width);
+                rotRow[rIndex]++;
+                rotSum++;
+                if (rotRow[rIndex] == 4)
+                {
+                    rotRow[rIndex] = 0;
+                    rotSum -= 4;
+                }
+            }
+
+            foreach (int rot in rotRow)
+            {
+                endRotValues.Add(rot);
+            }
+
+            foreach (int xVal in rowXValues)
+            {
+                endXValues.Add((xVal + shiftX) % width);
+            }
+        }
+
+        
+    }
+
+    private void UpdatePuzzleDisplay()
+    {
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                int index = board[i, j].x + width * i;
+                boardDisplay[index].GetComponent<SpriteRenderer>().sprite = sprites[board[i, j].spriteIndex];
+                boardDisplay[index].transform.eulerAngles = Vector3.forward * board[i, j].rotation * 90;
+            }
+        }
+
+        if (IsPuzzleComplete()) OnComplete();
+    }
+
+    private bool IsPuzzleComplete()
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                if (board[i, j].x != board[i, j].endX) return false;
+                if (board[i, j].rotation != board[i, j].endRotation) return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+public struct BillboardPuzzleTile
+{
+    public int x;
+    public int endX;
+    public int rotation;
+    public int endRotation;
+    public int spriteIndex;
+}
---
+++ Assets/Scripts/Puzzles/BillboardPuzzle/BillboardPuzzleButton.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class BillboardPuzzleButton : Interactable
+{
+    [SerializeField] private bool beeMode;
+    private GameObject bee;
+    private BeeControlAbility beeAbility;
+    private GameObject player;
+    private BoxCollider2D myCollider, beeCollider, playerCollider;
+    [SerializeField] private int row, col;
+
+    public override void OnInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void SetProximity()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private new void Start()
+    {
+        myCollider = GetComponent<BoxCollider2D>();
+        player = Player.ActivePlayer.gameObject;
+        if (Player.ActivePlayer.Ability is BeeControlAbility)
+        {
+            beeAbility = (BeeControlAbility)Player.ActivePlayer.Ability;
+            bee = (beeAbility).BeeRef.gameObject;
+            beeCollider = bee.GetComponent<BoxCollider2D>();
+            playerCollider = player.GetComponent<BoxCollider2D>();
+        }
+    }
+
+    void Update()
+    {
+        // Comment these next few lines out if you want the bee to follow the mouse instead of moving how it normally does.
+        /*if (bee != null)
+        {
+            bee.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            bee.transform.position -= Vector3.forward * bee.transform.position.z;
+        }*/
+        // temp input reading until entire input system is ready
+        if (!Input.GetKeyDown(KeyCode.E)) return;
+
+        if (beeMode && beeAbility.controllingBee)
+        {
+            if (myCollider.bounds.Contains(beeCollider.bounds.center))
+            {
+                BillboardPuzzle.Instance.ShiftRow(row);
+            }
+        }
+        else if (!beeAbility.controllingBee)
+        {
+            if (myCollider.bounds.Contains(playerCollider.bounds.center))
+            {
+                BillboardPuzzle.Instance.RotateCol(col);
+            }
+        }
+    }
+
+    
+}
