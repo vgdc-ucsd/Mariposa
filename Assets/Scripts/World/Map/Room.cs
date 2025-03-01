05b19e1be1e18ae448e16e26b50a58d105f499d1
DrakeFillinigm <drake.fillingim@gmail.com>
2025-02-28 19:16:06 -0800

    fixed compilation errors

--- Assets/Scripts/World/Map/Room.cs
+++ Assets/Scripts/World/Map/Room.cs
@@ -32,7 +32,7 @@ public class Room : MonoBehaviour
         {
             foreach (Transform child in roomContainer.transform)
             {
-                roomObjects.Add(child.gameObject);
+                //roomObjects.Add(child.gameObject);
             }
         }
     }
@@ -49,7 +49,7 @@ public class Room : MonoBehaviour
 
         if (!isPrefabRoom)
         {
-            StartCoroutine(LoadScene());
+            //StartCoroutine(LoadScene());
         }
         else
         {
@@ -66,7 +66,7 @@ public class Room : MonoBehaviour
     {
         if (!isPrefabRoom)
         {
-            StartCoroutine(UnloadScene());
+            //StartCoroutine(UnloadScene());
         }
         else
         {
