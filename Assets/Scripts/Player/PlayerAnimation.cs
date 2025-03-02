840018ecbf179b7b4f6c2c9a6df9a4f4793de720
MW1148 <Mason.Wong0325@gmail.com>
2025-03-01 22:36:02 -0800

    Volume Control Logic (missing saving), basic bgm and ambience implementation, prelim footsteps sfx implementation
    New test scene - "VolumeControlTestScene" tests the newly implemented features
    
    BGM and Ambience is controlled under a component in Main Camera
    - can be stopped and started
    - can change music / ambience with ChangeSound(String eventPath)
    
    Volume Control Logic can be found under Persistent object in test scene
    - Placeholder sliders created but can be swapped out easily by plopping into Settings object in inspector
    - 0.8 is normal volume and allows for range between 0 and 1
    - multiple categoies including master, music, sfx, dialogue
    
    Footsteps implementation found under MariposaPlayer object in test scene
    - added to the PlayerAnimation.cs file
    - can be controlled in inspector
    * new path input to specify footstep event
    * new Terrain Tag (NOTE: no implementation currently to detect tag of the ground collision, requires manual change)

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -1,9 +1,16 @@
+using System;
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
     SpriteRenderer playerSprite;
     Animator animator;
+    [SerializeField] String TerrainTag = "Default";
+    [SerializeField] String FootstepSoundEvent = "event:/sfx/player/footstep";
+
+    Boolean WasRunning;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -15,6 +22,11 @@ public class PlayerAnimation : MonoBehaviour
         // at rest
         if (Player.ActivePlayer.Movement.Velocity.sqrMagnitude <= 0.05f)
         {
+            if (WasRunning == true)
+            {
+                PlayFootstep();
+                WasRunning = false;
+            }
             animator.SetFloat("xVelocity", 0);
             return;
         }
@@ -24,11 +36,35 @@ public class PlayerAnimation : MonoBehaviour
         {
             animator.SetFloat("xVelocity", 1);
             playerSprite.flipX = true;
+            WasRunning = true;
         }
         else
         {
             animator.SetFloat("xVelocity", 1);
             playerSprite.flipX = false;
+            WasRunning = true;
         }
+    }
+
+    String MaterialCheck()
+    {
+        //TODO: need to be able to check what is on the ground
+        switch (TerrainTag)
+        {
+            case "Concrete":
+                return "Concrete";
+            case "Wood":
+                return "Wood";
+            default:
+                return "Default";
+        }
+    }
+
+    public void PlayFootstep()
+    {
+        EventInstance footstepInstance = RuntimeManager.CreateInstance(FootstepSoundEvent);
+        footstepInstance.setParameterByNameWithLabel("Terrain", MaterialCheck());
+        footstepInstance.start();
+        footstepInstance.release();
     }
 }
