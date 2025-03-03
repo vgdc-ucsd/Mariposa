62df0b1efbf35c61af67019c8f7753e7770383d6
Yoav Gutman <ygutman@ucsd.edu>
2025-03-02 21:12:10 -0800

    fixed all the edge cases for NPC.cs\n\nthis was a struggle honestly

--- Assets/Scripts/World/NPCs/NPC.cs
+++ Assets/Scripts/World/NPCs/NPC.cs
@@ -6,36 +6,48 @@ public class NPC : Interactable
 {
 
 	[SerializeField] private Dialogue dialogueNPC;
+	[SerializeField] private Dialogue finishDialogue;
 	private DialogueManager manager;
 	private bool isSpeaking = false;
 	private int count;
+	private Dialogue usedDialogue;
 
 	protected override void Start()
 	{
 		manager = DialogueManager.Instance;
 		count = 0;
+		usedDialogue = dialogueNPC;
 	}
 
     public override void OnInteract(IControllable controllable)
     {
-		if(count >= dialogueNPC.Conversation.Count)
+		if(count >= usedDialogue.Conversation.Count)
 		{
-			manager.DialogueWindow.SetActive(false);
-			count = 0;
-			isSpeaking = false;
-			PlayerController.Instance.ToggleMovementLock();
+			if(manager.finishedTypewriter)
+			{
+				manager.DialogueWindow.SetActive(false);
+				count = 0;
+				isSpeaking = false;
+				this.usedDialogue = finishDialogue;
+				PlayerController.Instance.ToggleMovementLock();
+			}
+			else
+			{
+				manager.TryAdvanceDialogue();
+			}
 		}
 		else if (count == 0)
 		{
-			manager.PlayDialogue(dialogueNPC);
+			manager.PlayDialogue(usedDialogue);
 			isSpeaking = true;
 			count++;
 			PlayerController.Instance.ToggleMovementLock();
 		}
 		else
 		{
+			if(manager.finishedTypewriter)
+				count++;
 			manager.TryAdvanceDialogue();
-			count++;
 		}
     }
 }
