b1ee3e2a0371e4bc8732512d761622ab97849fb4
Yoav Gutman <ygutman@ucsd.edu>
2025-05-02 14:20:03 -0700

    Committed DialogueParser at long last

---
+++ Assets/Scripts/UI/Dialogue/DialogueParser.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+public class DialogueOptionIntermediate
+{
+	public string text;
+	public List<string> impact;
+	public List<DialogueElementIntermediate> script;
+
+	public override string ToString() 
+	{
+		string output = "Option titled \"" + text + "\"\n";
+		output += "Impact: ";
+		foreach (string aspect in impact)
+		{
+			output += aspect + " ";
+		}
+		output += "Script:\n";
+		if(script != null)
+		{
+			foreach (DialogueElementIntermediate ele in script) 
+			{
+				if(ele != null)
+					output += ele.ToString();
+			}
+		}
+		else
+		{
+			output += "No script.\n";
+		}
+		return output;
+	}
+
+}
+
+public class DialogueElementIntermediate 
+{
+	public string name;
+	public string line;
+	public string sound;
+	public string icon;
+	public List<DialogueOptionIntermediate> options;
+
+	public override string ToString() 
+	{
+		string output = name + ": [" + line + "] <plays: " + sound + " looks: " + icon + ">\n";
+		if(options == null)
+		{
+			output += "No options.\n";
+		}
+		else if(options.Count == 0)
+		{
+			output += "No options.\n";
+		}
+		else 
+		{
+			output += options.Count + " option";
+			if(options.Count == 1)
+			{
+				output += ":\n";
+			}
+			else 
+			{
+				output += "s:\n";
+			}
+			foreach (var opt in options) 
+			{
+				if(opt != null)
+				{
+					output += opt.ToString();
+				}
+				else 
+				{
+					output += "Invalid option\n";
+				}
+			}
+		}
+		return output;
+	}
+}
+
+public class DialogueParser 
+{
+	private static IDeserializer deserializer = new DeserializerBuilder()
+		.WithNamingConvention(CamelCaseNamingConvention.Instance)
+		.IgnoreUnmatchedProperties()
+		.Build();
+	public static Dialogue ParseDialogue(string path)
+	{
+		/*FileStream file = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.Read);*/
+		string yml = File.ReadAllText(path);
+		return null;
+	}
+	static List<DialogueElementIntermediate> ParseDialogueTest(string path)
+	{
+		string yml = File.ReadAllText(path);
+
+		var p = deserializer.Deserialize<List<DialogueElementIntermediate>>(yml);
+
+		return p;
+		/*return returnable;*/
+	}
+
+	public static void SelfTest(string path)
+	{
+		var test = ParseDialogueTest(path);
+
+		Debug.Log("Length: " + test.Count.ToString());
+
+		foreach (DialogueElementIntermediate ele in test)
+		{
+			Debug.Log(ele.ToString());
+		}
+	}
+}
