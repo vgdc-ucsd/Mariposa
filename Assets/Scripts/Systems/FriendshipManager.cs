89bd73f423c2b970b1926f0377a3777bade3668d
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2025-05-20 03:05:09 -0700

    import unnamed hometown art, create sublevel prefab

--- Assets/Scripts/Systems/FriendshipManager.cs
+++ Assets/Scripts/Systems/FriendshipManager.cs
@@ -5,6 +5,7 @@ public class FriendshipManager : Singleton<FriendshipManager>
     private const int GOOD_THRESHOLD = 7;
     private int score = 0;
 
-    public void AddScore(int delta) => score += delta;
+    public void ChangeScore(int delta) => score += delta;
     public bool IsGoodScore() => score >= GOOD_THRESHOLD;
+    public bool CompareScore(int compareTo) => score >= compareTo;
 }
