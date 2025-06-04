60f277c6237be4392893d9702d0799ef832eb675
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2025-06-04 00:29:54 -0700

    lock player movement in boat cutscene

--- Assets/Scripts/UI/Dialogue/PlayBoatCutscene.cs
+++ Assets/Scripts/UI/Dialogue/PlayBoatCutscene.cs
@@ -19,6 +19,7 @@ public class PlayBoatCutscene : DialogueEvent
         while (timer < duration)
         {
             yield return new WaitForEndOfFrame();
+            PlayerController.Instance.SetMovementLock(true); // TODO: fix properly, improve dialogue 'end' flag
             timer += Time.deltaTime;
             PlayerController.Instance.ControlledPlayer.transform.SetParent(boat);
             boat.Translate(speed * Time.deltaTime * Vector3.right);
