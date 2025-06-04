997c209ceea0c969c90d23dc56b379914fe41775
Nathan Scott <122716087+ncscott95@users.noreply.github.com>
2025-06-03 23:49:57 -0700

    add unnamed pier boat cutscene

--- Assets/Scripts/UI/Dialogue/PlayBoatCutscene.cs
+++ Assets/Scripts/UI/Dialogue/PlayBoatCutscene.cs
@@ -1,10 +1,29 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayBoatCutscene : DialogueEvent
 {
+    [SerializeField] private Transform boat;
+    [SerializeField] private float speed;
+    [SerializeField] private float duration;
+
     public override void Trigger()
     {
-        // TODO: implement cutscene
-        Debug.Log("Sailing away");
+        StartCoroutine(MoveBoat());
+    }
+
+    IEnumerator MoveBoat()
+    {
+        float timer = 0;
+        while (timer < duration)
+        {
+            yield return new WaitForEndOfFrame();
+            timer += Time.deltaTime;
+            PlayerController.Instance.ControlledPlayer.transform.SetParent(boat);
+            boat.Translate(speed * Time.deltaTime * Vector3.right);
+        }
+		yield return FadeController.Instance.FadeOut();
+        SceneManager.LoadScene(0);
     }
 }
