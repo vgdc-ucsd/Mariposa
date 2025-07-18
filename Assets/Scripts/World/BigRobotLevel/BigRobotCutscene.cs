7dee89dfe83fa0b7547ceb23d7ddb5bebc7f9abc
Eatyboy <141588549+Eatyboy@users.noreply.github.com>
2025-07-18 01:38:32 -0700

    Added new assets and miscellaneous tweaks and touches

--- Assets/Scripts/World/BigRobotLevel/BigRobotCutscene.cs
+++ Assets/Scripts/World/BigRobotLevel/BigRobotCutscene.cs
@@ -11,7 +11,6 @@ public class BigRobotCutscene : MonoBehaviour, IInputListener
     [SerializeField] private Animator animator;
     [SerializeField] private Image image;
     [SerializeField] private int loopCount = 1;
-    [SerializeField] private float fadeTime = 0.0f;
 
     private int currentLoopCount = 0;
     public delegate void Callback();
@@ -26,6 +25,7 @@ public class BigRobotCutscene : MonoBehaviour, IInputListener
     public void OnDropAnimationFinished()
     {
         currentLoopCount = 0;
+        RuntimeManager.StudioSystem.setParameterByName(BigRobotLevel.MUSIC_PARAM, (int)BigRobotLevel.MusicSection.CHASE_START);
     }
 
     public void OnLoopAnimationFinished()
@@ -34,14 +34,19 @@ public class BigRobotCutscene : MonoBehaviour, IInputListener
         if (currentLoopCount >= loopCount)
         {
             animator.SetTrigger("EndIdleLoop");
-            RuntimeManager.StudioSystem.setParameterByName(BigRobotLevel.MUSIC_PARAM, (int)BigRobotLevel.MusicSection.CUTSCENE_END);
         }
+    }
+
+
+    public void OnFadeOut()
+    {
+        PlayerController.Instance.Unsubscribe(this);
+        endCallback();
     }
 
     public void OnAnimationEnd()
     {
-        PlayerController.Instance.Unsubscribe(this);
-        endCallback();
+        gameObject.SetActive(false);
     }
 
     [ContextMenu("Play Cutscene")]
