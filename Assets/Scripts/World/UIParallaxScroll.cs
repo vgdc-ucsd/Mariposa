786ccc0c4e5e98df972a377d9f9a3e3ac1a5cb29
Ethan <ethancschwartzman@gmail.com>
2025-08-02 12:16:24 -0700

    UI parallax

--- Assets/Scripts/World/UIParallaxScroll.cs
+++ Assets/Scripts/World/UIParallaxScroll.cs
@@ -10,49 +10,26 @@ public class UIParallaxScroll : MonoBehaviour
     private float startPos;
     private float length;
     [SerializeField] private RectTransform rectTransform;
-    [SerializeField] private PlayerMovement playerMovementInfo;
 
     void OnEnable()
     {
         rectTransform = GetComponent<RectTransform>();
-        // rectTransform.position = new(Camera.main.transform.position.x, rectTransform.position.y, rectTransform.position.z);
-        startPos = rectTransform.position.x + (50.0f * length / 2.0f);
-        length = rectTransform.rect.width;
-        StartCoroutine(GetPlayerMovement());
-    }
-
-    IEnumerator GetPlayerMovement()
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            playerMovementInfo = Player.ActivePlayer.gameObject.GetComponent<PlayerMovement>();
-            if (playerMovementInfo == null)
-            {
-                yield return null;
-            }
-            else
-            {
-                yield return new WaitForSeconds(0.1f);
-            }
-        }
-        Debug.Log("Player not found");
+        startPos = 0f;
+        length = rectTransform.rect.width / 3.0f;
     }
 
     void FixedUpdate()
     {
-        if (playerMovementInfo == null) return;
-
-        // new implementation; horizontally displace item from canvas, which moves with camera
         float distance = Camera.main.transform.position.x * parallaxEffect;
         float movement = Camera.main.transform.position.x * (1 - parallaxEffect);
 
-        rectTransform.position = new Vector3(startPos + distance, Camera.main.transform.position.y + yOffset, transform.position.z);
+        transform.localPosition = new Vector3(startPos + distance, yOffset, transform.position.z);
         
-        if (movement > startPos + length / 2.0f)
+        if (movement > (startPos + length))
         {
             startPos += length;
         }
-        else if (movement <  startPos - length / 2.0f)
+        else if (movement < (startPos - length))
         {
             startPos -= length;
         }
